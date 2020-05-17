1ec1b599c5172003047f82e25c2d132d25156d13
ahmad chatha <ahmadchatha@ahmads-MacBook-Pro.local>
2020-05-17 16:34:38 -0400

    adding schema file and paql query

---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE trades(
+	id 		    serial,
+	stock       varchar(20),
+	sell_in 	integer NOT NULL,
+	price		real,
+	gain		real,
+	PRIMARY KEY (id));
+
+
+INSERT INTO trades (stock, sell_in, price, gain) VALUES ('AAPL', 1, 302.0, 2.0);
+INSERT INTO trades (stock, sell_in, price, gain) VALUES ('AAPL', 7, 302.0, 35.0);
+INSERT INTO trades (stock, sell_in, price, gain) VALUES ('AAPL', 10, 302.0, -20.0);
+INSERT INTO trades (stock, sell_in, price, gain) VALUES ('MSFT', 1, 184.0, 4.0);
+INSERT INTO trades (stock, sell_in, price, gain) VALUES ('MSFT', 2, 184.0, 10.0);
+INSERT INTO trades (stock, sell_in, price, gain) VALUES ('TSLA', 1, 800.0, 200.0);
+INSERT INTO trades (stock, sell_in, price, gain) VALUES ('TSLA', 3, 800.0, -200.0);
+INSERT INTO trades (stock, sell_in, price, gain) VALUES ('TSLA', 7, 800.0, 400.0);
+INSERT INTO trades (stock, sell_in, price, gain) VALUES ('TSLA', 15, 800.0, -800.0);
